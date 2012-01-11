2324ccf9886848a2c5df66cc6307661e2da65175
Byron Smith <bsmith89@gmail.com>
2012-01-11 14:50:35 -0500

    Cleaned everything up nicely

--- seq.py
+++ seq.py
@@ -1,74 +1,125 @@
+# !usr/bin/env python
+"""Simple sequence data simulation
+
+This module contains methods for parsing fasta files of the correct
+format and outputting a simulated sequence data set.
+
+"""
 import sys
 import optparse
 import random
 
 DEFAULT_ALPHABET = ['A','C','G','T']
 
-def pars_fasta(fa_file, abund_data=False):
-    seqs = []
+def parse_fa(fa_file, abund_data=False):
+    """Take a FASTA file and return a list of seqs.
+
+    If abund_data == True (default = false) The list contains each
+    seq proportionally to its abundance.  This abundance data is read
+    from the FASTA file as the last number on the title line.  If the
+    last word on the title line is not a integer abundance is assumed
+    to be 1.
+
+    fa_file must be an open file object.
+
+    """
+    out_seqs = []
     curr_seq = ""
     curr_abund = 0
     for line in fa_file:
+        # If this is the title line, then add current seq to out_seqs
+        # and reset curr_seq and curr_abund.
         if line.startswith('>'):
-            if curr_seq != "":
-                seqs += [curr_seq]*curr_abund
+            if curr_seq == "": # if this is the very first title line
+                continue
+            seqs += [curr_seq]*curr_abund
             curr_seq = ""
-            try:
-                curr_abund = int(line.split()[-1]) # Seq abundances
-            except:
-                curr_abund = 1                 # should be an int
-                                               # at the
-                                               # end of the title
-                                               # line.
+            # Sequence abundances should be an integer at the end of
+            # the title line.
+            if len(line.split()) > 1:
+                try:
+                    curr_abund = int(line.split()[-1])
+                except ValueError:
+                    curr_abund = 1                
         else:
-            curr_seq += line.strip()
-    if curr_seq != "":
+            # Add the current line of sequence, removing white space
+            # and setting all letters uppercase.
+            curr_seq.join(line.strip().upper())
+    # Add the final sequence and return output
+    if curr_seq != "": # in case the file was empty
         seqs += [curr_seq]*curr_abund
-    return seqs
+    return out_seqs
 
-def sample(n, seqs):
-    out = []
+def sample_from_seqs(n, seqs_list, rand_seed = None):
+    """Choose with replacement n sequences from seqs_list.
+
+    Return a list of n sequences chosen randomly from seqs_list.  If
+    rand_seed is set, it is used as the seed value for sequence
+    picking.
+    
+    """
+    out_seqs = []
     for i in range(n):
-        out += [random.choice(seqs).upper()]
-    return out
+        seq = random.choice(seqs_list)
+        out_seqs += [seq]
+    return out_seqs
 
-def add_errors(e, seqs, alphabet = DEFAULT_ALPHABET):
-    max = 1.0/e
-    out = []
-    for seq in seqs:
-        obs_seq = ""
-        for nucl in seq:
-            obs_nucl = None
-            assert nucl in alphabet
-            if random.randrange(max) == 0:
+def simulate_errors(e, seqs_list, alphabet = DEFAULT_ALPHABET):
+    """Return a list of seqs where errors have been introduced.
+
+    Errors are introduced at a per nucleotide error rate of e.  All
+    erroneous nucleotides are picked from the character list alphabet
+    with the true nucleotide removed.
+    
+    """
+    # The maximum integer to be randomly generated should be no
+    # higher than error-rate^(-1) so that an error rate of 0.01
+    # means that integers should be randomly picked up to 100.
+    range_len = int(1.0/e)
+    out_seqs = []
+    for true_seq in seqs:
+        observed_seq = ""
+        for true_nucl in true_seq:
+            observed_nucl = None
+            # This assertion shouldn't really be necessary
+            assert true_nucl in alphabet
+            # if the random integer (from 0 to range_len-1) is 0
+            if random.randrange(range_len) == 0:
                 obs_nucl = random.choice(alphabet)
+                # The erronious nucleotide should never be the true
                 while obs_nucl == nucl:
                     obs_nucl = random.choice(alphabet)
             else:
-                obs_nucl = nucl
-            obs_seq += obs_nucl
-        out += [obs_seq]
-    return out
+                obs_nucl = true_nucl
+            obs_seq.join(obs_nucl)
+        out_seqs += [obs_seq]
+    return out_seqs
 
-def quick_data(filename, n=10000, e=0.01):
-    return add_errors(e, sample(n, pars_fasta(open(filename))))
+def quick_data(file_path, n=10000, e=0.01):
+    return simulate_errors(e, sample_from_seqs(n, parse_fa(open(file_path), abund_data = True)))
 
 if __name__ == '__main__':
     p=optparse.OptionParser()
     p.add_option('--fasta', '-f')
     p.add_option('--output', '-o')
-    p.add_option('--sample-size', '-n', default = '1000')
+    p.add_option('--sample-size', '-n', default = '10000')
     p.add_option('--error-rate', '-e', default = '0.01')
     opts, args = p.parse_args()
     if opts.fasta:
         infile = open(opts.fasta)
     else:
         infile = sys.stdin
-    if opts.out:
+    if opts.output:
         outfile = open(opts.output)
     else:
         outfile = sys.stdout
     error_rate = float(opts.error_rate)
-    sample_size = int(sample_size)
-
-
+    sample_size = int(opts.sample_size)
+    seq_data = simulate_errors(error_rate, sample_from_seqs(sample_size, parse_fa(infile)))
+    infile.close()
+    index = 0
+    for seq in seq_data:
+        seq_name = "seq%d" % index
+        outfile.write(">%s\n%s\n" % (seq_name, seq))
+        index += 1
+    outfile.close()
--- sum.py
+++ sum.py
@@ -2,93 +2,113 @@ import sys
 import optparse
 
 def list_seqs(seqs):
-    return count(seqs).keys()
+    """Returns a sorted list of all sequences in seqs.
 
-def diffs_matrix(seqs, max_dist=None):
-    out = {}
-    seqs = count(seqs).keys()
-    num_done = 0
-    for seq1 in seqs:
-        counts = {}
-        for seq2 in seqs:   
-            dist = None
-            if seq2 in out and seq1 in out[seq2]:
-                dist = out[seq2][seq1]
-            else:
-                dist = count_diffs(seq1, seq2)
-            if max_dist != None and dist > max_dist:
-                continue
-            else:
-                counts[seq2] = dist
-        out[seq1] = counts
-        num_done += 1
-        print("%s done %d" % (seq1, num_done))
-    return out
-
-def calc_centr_index(diffs_matrix, counts):
-    """Calculate the centrality index for each seq in diffs_matrix
-
-    The centrality index of seq1 is the total sum of seqs found
-    weighted by 1 over their distance from seq1.
-
-    Unfortunately this doesn't seem to be the solution for a sample
-    of size 10000.  I found that a 1% sequence had a centrality index
-    of only ~70, compared to ~1400 for a 99% abundance sequence.
-    To me, this means that the index is not independent of abundance,
-    but I would still expect it to become increasingly independent as
-    sample size -> inf.  This index should really be re-labeled,
-    because it is not really an index of centrality, although I wanted
-    it to be one.
-
-    A centrality index should be independent of sample size, and
-    reflect how closely "neighboring" sequences are distributed to
-    the perfectly binomial distribution predicted.
+    Somewhat computationally intensive, so if you every need
+    abundance data, you should probably use count(seqs) and cache
+    the resulting sequence list with its abundance data.
 
     """
-    out = {}
-    for seq1 in diffs_matrix.keys():
-        centr_index = 0
-        for seq2 in diffs_matrix[seq1]:
-            if seq2 == seq1:
-                continue
-            else:
-                centr_index += (1.0 / diffs_matrix[seq1][seq2])
-        out[seq1] = centr_index
-    return out
+    return sorted(count(seqs).keys())
 
-def count_diffs(seq1, seq2):
+def calc_diffs_matrix(seqs):
+    """Calculate the distances between all seqs.
+    
+    Returns a dictionary of dictionaries (or matrix) where
+    dict[seq1][seq2] is the number of nucleotide differences b/t
+    seq1 and seq2.
+    
+    """
+    out_matrix = {}
+    seqs = sorted(count(seqs).keys())
+    num_done = 0
+    for seq1 in seqs:
+        seq1_vers = {}
+        for seq2 in seqs:   
+            if seq2 in out_matrix and seq1 in out_matrix[seq2]:
+                seq1_vers[seq2] = out_matrix[seq2][seq1]
+            else:
+                seq1_vers[seq2] = nucl_diffs(seq1, seq2)
+        out_matrix[seq1] = seq1_vers
+        num_done += 1
+    return out_matrix
+
+def nucl_diffs(seq1, seq2):
+    """Return the number of loci with different nucleotides in seqs.
+
+    seq1 and seq2 must be the same length.
+    """
     count = 0
     assert len(seq1) == len(seq2)
-    for i in range(len(seq1)):
-        if seq1[i] != seq2[i]:
+    if seq1 == seq2:
+        return 0
+    for char1, char2 in zip(seq1, seq2)
+        if char1 != char2:
             count += 1
     return count
 
-def count(seqs):
-
-    out = {}
+def abund(seqs):
+    """Returns the abundance of each seq in a list of seqs."""
+    out_dict = {}
     for seq in seqs:
         if seq in out:
-            out[seq] += 1
+            out_dict[seq] += 1
         else:
-            out[seq] = 1
-    return out
+            out_dict[seq] = 1
+    return out_dict
 
-def rabund(seqs, size = None):
-    out = {}
-    seq_counts = count(seqs)
-    if size == None:
-        size = sum(seq_counts.values())
-    for seq in count(seq_counts):
-        out[seq] = float(seq_counts[seq])/size
-    return out
+def calc_index0(seqs, diffs_matrix = None, abund = None):
+    """Calculate the index0 for each seq in seqs.
+
+    This is just a prototype function, where the result is a dict
+    where every key points to a zero.
+
+    """
+    if diffs_matrix is None:
+        diffs_matrix = calc_diffs_matrix(seqs)
+    if abund is None:
+        abund = abund(seqs)
+    out_dict = {}
+    for seq1 in diffs_matrix.keys():
+        out_dict[seq1] = 0
+    return out_dict
+
+def calc_index1(seqs, diffs_matrix = None, abund = None):
+    """Calculate the index1 for each seq in seqs.
+
+    Index1 = The total number of sequences within dist=1 of seq.
+    """
+    if diffs_matrix is None:
+        diffs_matrix = calc_diffs_matrix(seqs)
+    if abund is None:
+        abund = abund(seqs)
+    out_dict = {}
+    for seq1 in diffs_matrix.keys():
+        for seq2 in diffs_matrix[seq1].keys():
+            if diffs_matrix[seq1][seq2] == 1:
+                out_dict[seq1] += abund[seq2]
+    return out_dict
+
+def calc_index2(seqs, diffs_matrix = None, abund = None):
+    """Calculate the index2 for each seq in seqs.
+
+    Index2 = The total number of sequences within dist<1 of seq
+    """
+    if diffs_matrix is None:
+        diffs_matrix = calc_diffs_matrix(seqs)
+    if abund is None:
+        abund = abund(seqs)
+    out_dict = {}
+    for seq1 in diffs_matrix.keys():
+        for seq2 in diffs_matrix[seq1].keys():
+            if diffs_matrix[seq1][seq2] <= 1:
+                out_dict[seq1] += abund[seq2]
+    return out_dict
 
 if __name__ == '__main__':
     p=optparse.OptionParser()
     p.add_option('--fasta', '-f')
     p.add_option('--output', '-o')
-    p.add_option('--sample-size', '-n', default = '1000')
-    p.add_option('--error-rate', '-e', default = '0.01')
     opts, args = p.parse_args()
     if opts.fasta:
         infile = open(opts.fasta)
@@ -98,7 +118,6 @@ if __name__ == '__main__':
         outfile = open(opts.output)
     else:
         outfile = sys.stdout
-    error_rate = float(opts.error_rate)
-    sample_size = int(sample_size)
 
-
+    infile.close()
+    outfile.close()
