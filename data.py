115030114af2c21c0ac408d04a0369c9f2d94d6d
Byron Smith <bsmith89@gmail.com>
2012-02-14 17:46:30 -0500

    Big changes, trying to make it more robust

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 
 """
 import random
-from scipy import array
+import scipy as sp
 from scipy.spatial.distance import hamming
 
 
@@ -17,59 +17,56 @@ class Seq():
     Also defines methods for comparing sequences
     
     """
-    def __init__(self, seq_str, abund, attrs = None):
-        self._seq = array(list(seq_str.strip().upper()))
-        self._abund = float(abund)
-        if attrs is None:
-            self._attrs = {}
-                    
-    def set_attr(self, attr, value):
-        self._attrs[attr] = value
-        
-    def get_attr(self, attr):
-        return self._attrs[attr]
+    def __init__(self, sequence, **kwargs):
+        self._sequence = sp.array(list(sequence))
+        for attr in kwargs.keys():
+            self.__setattr__(attr, kwargs[attr])
+    
+    def __iter__(self):
+        for nucl in self._sequence:
+            yield nucl
+    
+    def __len__(self):
+        return len(self._sequence)
+    
+    def __eq__(self, other):
+        try:
+            other_seq_array = other.get_seq_array()
+        except AttributeError:
+            other_seq_array = sp.array(list(other))
+        return all(self.get_seq_array() == other_seq_array)
     
     def get_attrs(self):
-        return self._attrs
+        attr_dict = dict(self.__dict__)
+        del attr_dict['_sequence']
+        return attr_dict
+            
+    def get_seq_array(self):
+        return self._sequence
     
-    def del_attr(self, attr):
-        del self._attrs[attr]
-        
-    def get_seq(self):
-        return self._seq
-
-    def __str__(self):
-        seq_str = ""
-        for nucl in self._seq:
-            seq_str += str(nucl)
-        return seq_str
-
-    def __len__(self):
-        return len(self._seq)
-
     def __lt__(self, other):
-        return float(self) < float(other)
-
-    def dist(self, other):
-        """Returns the hamming distance of self vs. other)"""
-        assert len(self) == len(other)
-        return hamming(self._seq, other._seq) * len(self)
+        return str(self) < str(other)
+    
+    def __dist__(self, other):
+        try:
+            other_seq_array = other.get_seq_array()
+        except AttributeError:
+            other_seq_array = sp.array(list(other))
+        return hamming(self.get_seq_array(), other_seq_array)
+        
+    def __str__(self):
+        return ''.join(self.get_seq_array())
     
     def __repr__(self):
-        return ("Seq(seq='%s', abund=%f, attrs=%s)" % (str(self), float(self), str(self._attrs)))
-        
-    def __eq__(self, other):
-        return str(self) == str(other)
-        
-    def __iadd__(self, value):
-        self._abund += float(value)
-        return self
-
-    def __add__(self, value):
-        return Seq(str(self), self._abund + float(value))
-        
-    def __float__(self):
-        return float(self._abund)
+        other_attrs = self.get_attrs()
+        out_str = "Seq('%s', **%s)" % (str(self), str(other_attrs))
+        return out_str
+    
+    def get_abund(self):
+        return self.abund
+    
+    def set_abund(self, value):
+        self.abund = float(value)
 
 
 class SeqList():
@@ -78,174 +75,132 @@ class SeqList():
     Also defines methods for analyzing the sequence sample
     
     """
-    def __init__(self, seqs = None):
-        # the dict attribute is a dictionary with the sequence
-        # string as the key and the Seq object as the value.
-        self._seq_dict = {}
+    def __init__(self, seq_list = []):
         self._seq_list = []
-        if seqs is not None:
-            for seq in seqs:
-                # add the sequence to the dictionary
-                if seq in self:
-                    self[seq] += seq._abund
-                else:
-                    try:
-                        abund = float(seq)
-                    except TypeError:
-                        abund = DEFAULT_ABUND
-                    try:
-                        attrs = seq.get_attrs()
-                    except TypeError:
-                        attrs = None
-                    self[seq] = Seq(str(seq), abund, attrs)
-            
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
-            
-    def __reversed__(self):
-        for i in range(len(self)-1, -1, -1):
-            yield self[i]
-            
-    def sort_by(self, attr, reverse = False):
-        working_seq_list = []
-        working_attr_list = []
-        sorted_seq_list = []
-        for seq in self:
-            working_seq_list += [str(seq)]
-            if attr == "abund":
-                working_attr_list += [float(seq)]
-            elif attr == "seq":
-                working_attr_list += [str(seq)]
-            else:
-                try:
-                    working_attr_list += [seq.get_attr(attr)]
-                except KeyError:
-                    working_attr_list += None
-        sorted_zipper = sorted(zip(working_attr_list, working_seq_list))
-        for tup in sorted_zipper:
-            sorted_seq_list += [str(tup[1])]
-        if reverse is not True:
-            self._seq_list = sorted_seq_list
-        else:
-            self._seq_list = list(reversed(sorted_seq_list))
-        
+        self.append(seq_list)
     
-    def seq_strs(self):
-        return self._seq_list
-
+    def __iter__(self):
+        return iter(self._seq_list)
+    
+    def __reversed__(self):
+        return reversed(self._seq_list)
+    
     def __repr__(self):
         seqs_str = ""
         for seq in self:
             seqs_str += "%s\n" % repr(seq)
-        return "SeqList(%s)" % seqs_str
+        repr_str = "SeqList(\n%s)" % seqs_str
+        return repr_str
     
     def __str__(self):
-        seqs_str = ""
+        header_str = ""
+        body_str = ""
+        curr_index = 0
+        attr_lists = {}
         for seq in self:
-            seqs_str += "%s\t%f\n" % (str(seq), float(seq))
-        return "SeqList\n(\n%s)" % seqs_str
+            attrs_for_this_seq = seq.get_attrs()
+            for attr in attrs_for_this_seq:
+                if attr not in attr_lists:
+                    attr_lists[attr] += ["NA"] * curr_index
+            for attr in attr_lists:
+                try:
+                    value_for_this_seq = attrs_for_this_seq[attr]
+                except KeyError:
+                    value_for_this_seq = "NA"
+                finally:
+                    attr_lists[attr] += [value_for_this_seq]
+            curr_index += 1
+        for attr in attr_lists:
+            header_str += "%s\t" % attr
+        header_str += "sequence"
+        attr_zip = zip(*attr_lists.values())
+        for line in attr_zip:
+            body_str += "\n%s" % "\t".join(line)
+        return header_str + body_str
+                
+    def fasta(self):
+        fasta_str = ""
+        seq_count = 1
+        for seq in self:
+            title_line = ">seq%d" % seq_count
+            attrs = seq.get_attrs()
+            seq_str = str(seq)
+            for attr in attrs:
+                title_line += "|%s=%s" % (attr, str(attrs[attr]))
+            fasta_str += "%s\n%s\n" % (title_line, seq_str)
+        return fasta_str
     
     def __contains__(self, seq):
-        """Check if the sequence or Seq object are in SeqList."""
-        if str(seq) in self._seq_list:
+        if seq in self._seq_list:
             return True
-        else:
-            return False
-        
-    def __getitem__(self, key):
-        """Return the Seq object which has the same sequence as key
-        
-        """
+        return False
+    
+    def __getitem__(self, key_or_index):
         try:
-            return self._seq_dict[self._seq_list[key]]
-        except TypeError:
-            return self._seq_dict[str(key)]
+            index = self.index(key_or_index)
+        except ValueError:
+            try:
+                assert (index % 1.0 == 0)
+            except AssertionError:
+                raise KeyError("No sequence %s found in this SeqList" % str(key_or_index))
+            finally:
+                index = key_or_index
+        finally:
+            return self._seq_list[index]
+    
+    def __setitem__(self, key_or_index, seq):
+        if self.contains(key_or_index):
+            self._seq_list[self.index(key_or_index)] = seq
+        elif key_or_index % 1.0 == 0.0:
+            self._seq_list[key_or_index] = seq
+        else:
+            raise KeyError("No ")
     
     def __len__(self):
-        return len(self._seq_dict)
+        return len(self._seq_list)
     
-    def __setitem__(self, key, value):
-        """Add/Replace the item at str(key) with value."""
-        self._seq_dict[str(key)] = value
-        if not value in self._seq_list:
-            self._seq_list.append(str(value))
-        return self
-        
-    def __iadd__(self, seq_object):
-        """Add a single Seq object to self."""
-        if seq_object in self:
-            self[seq_object] = Seq(str(seq_object),
-                                   self[seq_object]._abund +
-                                       float(seq_object))
-        else:
-            self[seq_object] = seq_object
-        return self
+    def _add_one_seq(self, seq):
+        pass
     
-    def index(self, value):
-        assert value in self
-        return self._seq_list.index(str(value))
+    def __iadd__(self, seq_or_seqlist):
+        # Pseudocode:
+        # if seq_or_seqlist is a seqlist:
+        #     for seq in seqlist:
+        #         self._add_one_seq(seq)
+        # else:
+        #     self._add_one_seq(seq)
+        pass
     
-    def append(self, seq_objects):
-        """Add a list of seqs to self."""
-        for seq in seq_objects:
-            self += seq
-        return self
-
-    def total_abund(self):
-        running_total = 0.0
-        for seq in self:
-            running_total += float(seq)
-        return running_total
-        
-    def norm(self):
-        """Return a SeqList with normalized abundances, Total = 1.0"""
-        out_seq_list = SeqList()
-        abund_total = self.total_abund()
-        for seq in self:
-            out_seq_list += Seq(seq_str = str(seq), abund = float(seq) / abund_total)
-        return out_seq_list
-            
+    def index(self):
+        pass
+    
+    def append(self, seq_or_seqlist):
+        self += seq_or_seqlist
+    
+    def abund_total(self):
+        pass
+    
+    def normalize(self):
+        pass
+    
     def random_seq(self):
-        """Return a random Seq object from self.
-        
-        This algorithm could probably be sped up a good deal but
-        for now I don't need to.  Sampling doesn't seem to be the
-        bottleneck.
-        
-        """
-        abund_total = self.total_abund()
-        index = random.random()
-        adjusted_index = index * abund_total
-        tally = 0.0
-        for seq in self:
-            tally += float(seq)
-            if tally > adjusted_index:
-                return Seq(str(seq), abund = 1)
+        pass
+    
+    def sort_by(self, *args):
+        pass
+    
+    def copy(self):
+        pass
+    
+class SeqMatrix():
+    """Encodes all-by-all relationships for a list of sequences.
+    
+    """
+    def __init__(self):
+        pass
 
 def parse_file(fasta):
     """Takes a open file object and returns a Seqs object.
     
     """
-    seqs = SeqList()
-    curr_seq = ""
-    curr_abund = 0.0
-    for line in fasta:
-        if line[0] == ">":
-            # add the current sequence to seqs
-            if curr_seq is not "":
-                seqs += Seq(curr_seq, curr_abund)
-            # reset curr_*
-            curr_seq = ""
-            curr_abund = 0.0
-            # now, get the current abundance of this new sequence
-            try:
-                curr_abund = float(line.split()[-1])
-            except (ValueError, IndexError):
-                # There is no abundance on the title line
-                curr_abund = DEFAULT_ABUND
-        else:
-            # append the sequence to curr_seq
-            curr_seq += line.strip().upper()
-    seqs += Seq(curr_seq, curr_abund)
-    return seqs
+    pass
